694e4cf0c52ffee11e9d584603f38bdc17260295
d33ppo <mzadib1234@gmail.com>
2023-12-10 01:28:46 +0800

    delete top message

--- src/viva2q2/Viva2q2.java
+++ src/viva2q2/Viva2q2.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package viva2q2;
 import java.util.Arrays;
 public class Viva2q2 {
